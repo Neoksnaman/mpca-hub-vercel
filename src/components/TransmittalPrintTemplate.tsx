3ce2353342dc0ce7cf2f3d168d74723bd5fb325f
Neoksnaman <christianbaccay11@gmail.com>
2026-05-25 16:02:49 +0800

    Update transmittal address wording

--- src/components/TransmittalPrintTemplate.tsx
+++ src/components/TransmittalPrintTemplate.tsx
@@ -17,7 +17,7 @@ const SingleSlip = ({ transmittal, client, staffMember, logoUrl, copyType, forma
                     )}
                 </div>
                 <div className="text-right text-[9px] font-bold leading-tight italic pt-1">
-                    <p>Unit 501, West Insula Condominium,</p>
+                    <p>Unit 301, West Insula Building,</p>
                     <p>#135 West Avenue Brgy. Bungad, Quezon City</p>
                     <p>Tel No. (02) 8800-5415</p>
                 </div>
